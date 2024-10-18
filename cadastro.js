cd0fa2e1a0a9cccee472540c3736db8132d5fb38
GUILHERME SERAFIM DE LIMA <guilherme.serafim@alunoescolassociais.org.br>
2024-10-18 07:39:14 -0300

    fiz a validaçao do email falta so a validaçao do cpf

--- cadastro.js
+++ cadastro.js
@@ -47,4 +47,93 @@ const pesquisarcep = async() => {
 }
 // ADICIONAR ESCUTADOR PARA EXECUTAR CONSUMO DE API DA ViaCep
 document.getElementById('cep').addEventListener('focusout', pesquisarcep);
+
+// BLOCO PARA VALIDAÇAO DE EMAIL
+// --------------------------------------------------------------------
+function chegarEmail(){
+    if(document.forms[0].email.value == "" || 
+       document.forms[0].email.value.indexOf('@') == -1 ||
+       document.forms[0].email.value.indexOf('.') == -1)
+       {
+        alert("Por favor infrome um email valido");
+        return false;
+    }else{
+        alert("email informado com sucesso!");
+        document.getElementById('email').innerHTML = document.forms[0].email.value 
+    }
+}
+// --------------------------------------------------------------------
+
+// CODIGO DE VERICAÇAO DE EMAIL DIGITADO
+// --------------------------------------------------------------------
+
+function verifica(){
+    if(document.forms[0].email.value == 0){
+        alert("Por favor, corno, informe um E-mail");
+        document.frmEnvia.email.focus();
+        return false;
+    }
+    return true;
+}
+// --------------------------------------------------------------------
+ //validaçao para validaçao de acessar 
+
+ // VALIDA DE CPF 
+
+    // Adicionar escutador à página 
+    document.getElementById("cpfform").addEventListener("submit", function(event){
+        event.preventDefault();
+    
+        const cpf = document.getElementById("cpf").value;
+        const msg = document.getElementById("message")
+    
+        if(validarCPF(cpf)){
+            msg.textContent = "O CPF é válido";
+            msg.style.color = "green"; 
+        }else{
+            msg.textContent = "O CPF é inválido";
+            msg.style.color = "red";
+        }
+    }
+    );
+        //  Função de cálculo  de validação do CPF 
+        function validarCPF(cpf){
+        cpf = cpf.replace(/[^\d]+/g, ''); // Remove caracteres não númericos    
+    
+        //  Verificar se o valor infromado contem 11 dígitos e se todos são dígitos iguais 
+        if(cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)){
+        return false;
+        }   
+        let soma = 0;
+        let resto; 
+    
+        // validação do primeiro digito verificação
+        for(let i = 1; i <= 9; i++){
+            soma += parseInt(cpf.substring(i-1, i)) * (11- i);
+        }
+        resto = (soma * 10) % 11;  
+        if((resto === 10) || (resto === 11)){
+            resto = 0 
+        }
+        
+        if(resto !== parseInt(cpf.substring(9, 10))){
+            return false;
+          
+        }
+        soma = 0
+    
+        //validar 11 digitos do CPF - digitos verificador
+        for(let i = 1; i <= 10; i++){
+            soma += parseInt(cpf.substring(i-1, i)) * (12 - i); 
+        }
+        resto = (soma * 10 ) % 11; 
+        if((resto === 10 ) || (resto === 11)){
+            resto = 0;
+        }
+        if(resto !== parseInt(cpf.substring(10, 11))){
+            return false; 
+        }
+        return true; 
+    }
+
  
